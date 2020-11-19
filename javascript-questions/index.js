73b005aa5e430bde16bb42ed8f56f60bfddcc0b0
jsphkm <jsphkm12@gmail.com>
2020-11-18 21:46:33 -0500

    Add answers

--- javascript-questions/index.js
+++ javascript-questions/index.js
@@ -10,8 +10,14 @@ const superHeroes = require('./super-heroes.json');
  * ]
  */
 const getDCHeroes = (heroes) => {
-  return heroes;
-}
+  const res = [];
+  heroes.forEach((each) => {
+    if (each.publisher === 'DC Comics') {
+      res.push(each);
+    }
+  });
+  return res;
+};
 
 /**
  * Converts the character property into an array
@@ -27,8 +33,11 @@ const getDCHeroes = (heroes) => {
  * ]
  */
 const convertCharactersToArray = (heroes) => {
+  heroes.forEach((each, idx, arr) => {
+    arr[idx].characters = each.characters.split(',').map(val => val.trim());
+  });
   return heroes;
-}
+};
 
 /**
  * Coverts super hero data so it is grouped the publisher property
@@ -44,8 +53,16 @@ const convertCharactersToArray = (heroes) => {
  * }
  */
 const groupByPublisher = (heroes) => {
-  return heroes;
-}
+  const ans = {};
+  heroes.forEach((each) => {
+    if (each.publisher in ans) {
+      ans[each.publisher].push(each.superhero);
+    } else {
+      ans[each.publisher] = [each.superhero];
+    }
+  });
+  return ans;
+};
 
 /**
  * Returns and array of DC Comic heroes that have more than 1 character
@@ -61,12 +78,18 @@ const groupByPublisher = (heroes) => {
  * ]
  */
 const getDCHeroesWithMoreThanOneCharacter = (heroes) => {
-  return heroes;
-}
+  const ans = [];
+  heroes.forEach((each) => {
+    if (each.characters.length > 1) {
+      ans.push(each);
+    }
+  });
+  return ans;
+};
 
 module.exports = {
   convertCharactersToArray,
   getDCHeroes,
   getDCHeroesWithMoreThanOneCharacter,
   groupByPublisher,
-}
+};
